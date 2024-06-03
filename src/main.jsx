eef5ce9152e07faa145c68a4a7c760042dbaab11
BRUNOPUMA <122190257+Brunitozz@users.noreply.github.com>
2024-06-02 22:36:04 -0500

    login redone

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Login from './Screens/Login/Login.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-    {{/*
-      <App />
-  */}}
-    <Login />
+    <App />
   </React.StrictMode>,
-)
+);
