e21180c6d0cb391169e4fcc8a4031cb40d02da9b
BRUNOPUMA <122190257+Brunitozz@users.noreply.github.com>
2024-06-29 21:31:05 +0000

    sidebar done

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,22 @@ import { Register } from "./Screens/Register/Register.jsx";
 import { NavLayout } from './Components/NavLayout/NavLayout.jsx'
 import { EditarPerfil } from "./Screens/EditarPerfil/EditarPerfil.jsx";
 import { Publicar } from "./Screens/Publicar/Publicar.jsx";
+import {SideBarCuenta,SideBarPerfil} from "./Components/SideBar/SideBar.jsx";
 // Importa otros componentes según sea necesario
 
 export const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<NavLayout />} >
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="edit-profile/:id" element={<EditarPerfil />} />
-          {/* NOTE: Ruta para probar el componente Publicar */}
-          <Route path="post" element={<Publicar />} />
-          {/* Define otras rutas según sea necesario */}
-        </Route>
-      </Routes>
-    </>
+    <div className="flex">
+      <div className="w-3/4 p-4">
+        <Routes>
+          <Route path='/' element={<NavLayout />} >
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="edit-profile/:id" element={<EditarPerfil />} />
+            <Route path="post" element={<Publicar />} />
+          </Route>
+        </Routes>
+      </div>
+    </div>
   );
 }
