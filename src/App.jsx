84ac3947ec7369ed28dd8fcd62fba263c099f07d
Kiltom Adolfo <kiltomadolfo@gmail.com>
2024-06-29 16:30:23 -0500

    solucionk

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,10 @@ import { EditarPerfil } from "./Screens/EditarPerfil/EditarPerfil.jsx";
 import { Publicar } from "./Screens/Publicar/Publicar.jsx";
 import { Amigos } from "./Screens/Amigos/VerAmigos.jsx";
 import { Libros } from "./Screens/LibrosFavoritos/VerLibros.jsx";
-import {Navbar} from "./Components/Navbar/Navbar.jsx";
-import {MainFeed} from "./Screens/MainFeed/MainFeed.jsx";
 // Importa otros componentes según sea necesario
 
 export const App = () => {
   return (
-    <div className="app">
-      <Navbar/>
-      <MainFeed/>
 
       <Routes>
         <Route path="/">
@@ -28,6 +23,5 @@ export const App = () => {
           {/* Define otras rutas según sea necesario */}
         </Route>
       </Routes>
-    </div>
   );
 };
