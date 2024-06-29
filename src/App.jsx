bf323dc5fadb198d6541c392e7e2b77739081f24
Kiltom Adolfo <kiltomadolfo@gmail.com>
2024-06-29 17:01:24 -0500

    MainFeed funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,26 @@ import { EditarPerfil } from "./Screens/EditarPerfil/EditarPerfil.jsx";
 import { Publicar } from "./Screens/Publicar/Publicar.jsx";
 import { Amigos } from "./Screens/Amigos/VerAmigos.jsx";
 import { Libros } from "./Screens/LibrosFavoritos/VerLibros.jsx";
+import MainFeed from "./Screens/MainFeed/MainFeed.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
 // Importa otros componentes según sea necesario
 
 export const App = () => {
-  return (   
+  return (
+    <>
+      <Navbar />
       <Routes>
-        <Route path="/">
-          <Route path="register" element={<Register />} />
-          <Route path="login" element={<Login />} />
-          <Route path="edit-profile/:id" element={<EditarPerfil />} />
-          <Route path="amigos" element={<Amigos />} />
-          <Route path="libros" element={<Libros />} />
-          {/* NOTE: Ruta para probar el componente Publicar */}
-          <Route path="post" element={<Publicar />} />
-          {/* Define otras rutas según sea necesario */}
-        </Route>
+        <Route path="/" element={<MainFeed />} />
+        <Route path="main" element={<MainFeed />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="edit-profile/:id" element={<EditarPerfil />} />
+        <Route path="amigos" element={<Amigos />} />
+        <Route path="libros" element={<Libros />} />
+        {/* NOTE: Ruta para probar el componente Publicar */}
+        <Route path="post" element={<Publicar />} />
+        {/* Define otras rutas según sea necesario */}
       </Routes>
+    </>
   );
 };
