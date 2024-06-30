981ceae03357561206d69f96b690e6d3386a59fd
BrayanAlq <145175308+BrayanAlq@users.noreply.github.com.>
2024-06-30 01:06:07 -0500

    add hook for VerAmigos

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import { Login } from "./Screens/Login/Login.jsx";
 import { Register } from "./Screens/Register/Register.jsx";
 import { EditarPerfil } from "./Screens/EditarPerfil/EditarPerfil.jsx";
 import { Publicar } from "./Screens/Publicar/Publicar.jsx";
-import { Amigos } from "./Screens/Amigos/VerAmigos.jsx";
+import Amigos from "./Screens/Amigos/VerAmigos.jsx";
 import { Libros } from "./Screens/LibrosFavoritos/VerLibros.jsx";
 import MainFeed from "./Screens/MainFeed/MainFeed.jsx";
 import Navbar from "./Components/Navbar/Navbar.jsx";
-import { Solicitudes  } from "./Screens/Solicitudes/Solicitudes.jsx";
-import { PaginaLibro } from "./Screens/PaginaLibro/PaginaLibro.jsx"
+import { Solicitudes } from "./Screens/Solicitudes/Solicitudes.jsx";
+import { PaginaLibro } from "./Screens/PaginaLibro/PaginaLibro.jsx";
 import { MainLayout } from "./Components/MainLayout/MainLayout.jsx";
 import { NavLayout } from "./Components/NavLayout/NavLayout.jsx";
 // Importa otros componentes según sea necesario
@@ -21,10 +21,11 @@ export const App = () => {
         <Route path="/main" element={<MainLayout />}>
           <Route path="solicitudes" element={<Solicitudes />} />
           <Route path="libros/:idBook" element={<PaginaLibro />} />
+          <Route path="amigos" element={<Amigos />} />
         </Route>
-        <Route path="/init" element={ <NavLayout />}>
-          <Route path="login" element={<Login />}/>
-          <Route path="register" element={<Register />}/>
+        <Route path="/init" element={<NavLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
         </Route>
       </Routes>
     </>
--- src/Screens/Amigos/VerAmigos.jsx
+++ src/Screens/Amigos/VerAmigos.jsx
@@ -1,23 +1,27 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import Amigo from "./Amigo";
 import defaultImage from "../../icons/default-icon-profile.png";
+import useFriends from "../../hooks/useFriends";
 
-const friends = [
-  { name: "Amigo 1", image: defaultImage },
-  { name: "Amigo 2", image: defaultImage },
-  { name: "Amigo 3", image: defaultImage },
-  { name: "Amigo 4", image: defaultImage },
-  { name: "Amigo 5", image: defaultImage },
-  { name: "Amigo 6", image: defaultImage },
-];
+const Amigos = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const id = queryParams.get("id");
 
-export const Amigos = () => {
+  const { friends, loading, error } = useFriends(id);
+
   return (
     <div className="p-6">
       <p className="text-center text-2xl font-bold mb-6">Amigos</p>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
         {friends.map((friend, index) => (
-          <Amigo key={index} name={friend.name} image={friend.image} />
+          <Amigo
+            key={index}
+            id_user={friend.id_user}
+            name={friend.name}
+            image={defaultImage}
+          />
         ))}
       </div>
     </div>
