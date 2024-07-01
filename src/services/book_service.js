4fe5c3a83501c1a13c435289c6f4d53f2765b3af
Brunitozz <122190257+Brunitozz@users.noreply.github.com>
2024-06-30 22:34:27 -0500

    usuario y libros en la barra de busqueda

--- src/services/book_service.js
+++ src/services/book_service.js
@@ -3,12 +3,12 @@ import { api } from "../constants/api";
 const api_book = `${api}/book`
 
 const getBooks = async () => {
-  const response = await fetch(`${api_book}`);
+  const response = await axios.get(api_book);
   if (!response.ok) {
     throw new Error('Failed to fetch books');
   }
-  const data = await response.json();
-  return data;
+  //const data = await response.json();
+  return response;
 }
 
 export { getBooks }
